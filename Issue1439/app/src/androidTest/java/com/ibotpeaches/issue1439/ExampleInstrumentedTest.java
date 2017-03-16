8742c2ee191643cc5e17f00e8dcd86d4d2d05144
Connor Tumbleson <me@connortumbleson.com>
2017-03-16 08:21:22 -0400

    Added Apktool#1439 test apk

---
+++ Issue1439/app/src/androidTest/java/com/ibotpeaches/issue1439/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.ibotpeaches.issue1439;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.ibotpeaches.issue1439", appContext.getPackageName());
+    }
+}
