8a0baaafaf5c52c122f097ff33df177a17ffa11c
Connor Tumbleson <connor.tumbleson@gmail.com>
2023-09-06 06:10:51 -0400

    feat: add issue 2836 sample

---
+++ Issue2836/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.ibotpeaches.issue2836"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.ibotpeaches.issue2836"
+        minSdk = 33
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("com.google.android.material:material:1.9.0")
+}
