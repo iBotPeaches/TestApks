1286a92d2feaa73e758804c2f72fefa121b7a326
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-03-20 21:16:21 -0400

    feat: attempt at issue 3533 replication

---
+++ Issue3533/app/src/main/java/com/ibotpeaches/issue3533_2/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.ibotpeaches.issue3533_2.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
